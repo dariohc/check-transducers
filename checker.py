141770731ef70f306cc787f5dbb180be5885c931
Dario Hermida <dariohc@gmail.com>
2015-10-30 14:57:52 +0100

    go
    go

--- checker.py
+++ checker.py
@@ -5,7 +5,7 @@ line_limit = 10000
 line_counter = 0
 my_serial_list = []
 
-with open('Transducers - Copy.csv', newline='') as csvfile:
+with open('Transducers.csv', newline='') as csvfile:
     spamreader = csv.DictReader(csvfile)
     for row in spamreader:
         # print(row)
@@ -19,3 +19,4 @@ for index in range(len(my_serial_list)):
     if my_serial_list.count(my_serial_list[index]) > 1 and my_serial_list[index] != '0':
         print('there is something wrong with:', my_serial_list[index])
 input('If Jappy with the result, press any key to finish')
+
