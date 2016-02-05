742406d0343c9fb9febf51708243f4e702a53c3f
Dario Hermida <dariohc@gmail.com>
2016-02-05 11:04:06 +0100

    update
    updated

--- checker.py
+++ checker.py
@@ -8,15 +8,11 @@ my_serial_list = []
 with open('Transducers.csv', newline='') as csvfile:
     spamreader = csv.DictReader(csvfile)
     for row in spamreader:
-        # print(row)
-        # print(row['Serial Number'])
         my_serial_list.append(row['Serial Number'])
         line_counter += 1
 print('Total lines read:', len(my_serial_list))
 for index in range(len(my_serial_list)):
-    # print('estoy viendo el elemento', my_serial_list[index])
-    # print(my_serial_list.count(my_serial_list[index]))
     if my_serial_list.count(my_serial_list[index]) > 1 and my_serial_list[index] != '0':
         print('there is something wrong with:', my_serial_list[index])
-input('If Jappy with the result, press any key to finish')
+input('If Noppen is happy with the result, press any key to finish')
 
